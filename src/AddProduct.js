6c401c22e33a2a6bff47a1bd58db12d3b76dfdd6
Aatman Pradhan <aatmansp@rediffmail.com>
2021-09-05 00:29:23 +0530

    Add Product complete with backend

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -3,6 +3,11 @@ import './AddProduct.css';
 import {db} from './firebase';
 import { useState} from "react";
 
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
 function AddProduct() {
 
     const [title,setTitle]=useState();
@@ -10,9 +15,39 @@ function AddProduct() {
     const [price,setPrice]=useState();
     const [rating,setRating]=useState();
 
+    const [open, setOpen] = useState(false);
+
     const addNewProduct=((e)=>{
         e.preventDefault();
+
+        var productCollection=db.collection('products');
+
+        productCollection.add({
+            title:title,
+            image:imageUrl,
+            price:parseInt(price),
+            rating:parseInt(rating)
+        }).then(()=>{
+            setTitle("");
+            setImageUrl("");
+            setPrice("");
+            setRating("");
+
+            setOpen(true);
+        })
+
+
+
+        
     })
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+    };
 
     return (
         <div className="addProduct">
@@ -32,6 +67,24 @@ function AddProduct() {
                         
                         <h5></h5>
                         <button onClick={addNewProduct}>Add Product</button>
+
+                        <Snackbar
+                            anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'center',
+                            }}
+                            open={open}
+                            autoHideDuration={6000}
+                            onClose={handleClose}
+                            message="Product Added"
+                            action={
+                            <React.Fragment>
+                                <IconButton className="addProduct__SnakbarCloseBtn" size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                                <CloseIcon fontSize="small" />
+                                </IconButton>
+                            </React.Fragment>
+                            }
+                        />
                     </form>
         </div>
     )
