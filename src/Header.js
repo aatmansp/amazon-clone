d79537b119c07ab13efe6bff1361fdf4a750ceb7
Aatman Pradhan <aatmansp@rediffmail.com>
2022-11-01 12:21:14 +0530

    Header Catagories length now changed dynamically depending on option length

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import {db} from './firebase';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { Select, MenuItem} from '@material-ui/core';
 
 function Header() {
 
@@ -23,6 +23,8 @@ function Header() {
             auth.signOut();
         }
     }
+
+    const [selectedCatagory, setSelectedCatagory]= useState("search-alias=all");
 
     useEffect(()=>{
         var addressCollection=db.collection('users').doc(user?.uid).collection('addresses');
@@ -69,58 +71,58 @@ function Header() {
 
             <div className="header__search">
                 <div className="header__catagoriesDropdownContainer">
-                    {/* <div className="header__catagoriesText">
-                        <span>All</span>
-                        <ArrowDropDownIcon/>
-                    </div> */}
-                    <div className="header__catagoriesDropdown">
-                        <select name="Product Catagories" id="catagories">
-                            <option selected="selected" value="all">All </option>
-                            <option value="search-alias=alexa-skills">Alexa Skills</option>
-                            <option value="search-alias=amazon-devices">Amazon Devices</option>
-                            <option value="search-alias=fashion">Amazon Fashion</option>
-                            <option value="search-alias=amazon-pharmacy">Amazon Pharmacy</option>
-                            <option value="search-alias=appliances">Appliances</option>
-                            <option value="search-alias=mobile-apps">Apps &amp; Games</option>
-                            <option value="search-alias=baby">Baby</option>
-                            <option value="search-alias=beauty">Beauty</option>
-                            <option value="search-alias=stripbooks">Books</option>
-                            <option value="search-alias=automotive">Car &amp; Motorbike</option>
-                            <option value="search-alias=apparel">Clothing &amp; Accessories</option>
-                            <option value="search-alias=collectibles">Collectibles</option>
-                            <option value="search-alias=computers">Computers &amp; Accessories</option>
-                            <option value="search-alias=todays-deals">Deals</option>
-                            <option value="search-alias=electronics">Electronics</option>
-                            <option value="search-alias=furniture">Furniture</option>
-                            <option value="search-alias=lawngarden">Garden &amp; Outdoors</option>
-                            <option value="search-alias=gift-cards">Gift Cards</option>
-                            <option value="search-alias=grocery">Grocery &amp; Gourmet Foods</option>
-                            <option value="search-alias=hpc">Health &amp; Personal Care</option>
-                            <option value="search-alias=kitchen">Home &amp; Kitchen</option>
-                            <option value="search-alias=industrial">Industrial &amp; Scientific</option>
-                            <option value="search-alias=jewelry">Jewellery</option>
-                            <option value="search-alias=digital-text">Kindle Store</option>
-                            <option value="search-alias=luggage">Luggage &amp; Bags</option>
-                            <option value="search-alias=luxury-beauty">Luxury Beauty</option>
-                            <option value="search-alias=dvd">Movies &amp; TV Shows</option>
-                            <option value="search-alias=popular">Music</option>
-                            <option value="search-alias=mi">Musical Instruments</option>
-                            <option value="search-alias=office-products">Office Products</option>
-                            <option value="search-alias=pets">Pet Supplies</option>
-                            <option value="search-alias=instant-video">Prime Video</option>
-                            <option value="search-alias=speed-store">Same-Day Delivery</option>
-                            <option value="search-alias=shoes">Shoes &amp; Handbags</option>
-                            <option value="search-alias=software">Software</option>
-                            <option value="search-alias=sporting">Sports, Fitness &amp; Outdoors</option>
-                            <option value="search-alias=specialty-aps-sns">Subscribe &amp; Save</option>
-                            <option value="search-alias=home-improvement">Tools &amp; Home Improvement</option>
-                            <option value="search-alias=toys">Toys &amp; Games</option>
-                            <option value="search-alias=under-ten-dollars">Under ₹500</option>
-                            <option value="search-alias=videogames">Video Games</option>
-                            <option value="search-alias=watches">Watches</option>
-                            
-                        </select>
-                        <ArrowDropDownIcon />
+                    <div className="header__catagoriesDropdownDiv">
+
+                        <Select
+                            className="header__catagoriesDropdown"
+                            autoWidth
+                            value={selectedCatagory}
+                            onChange={(e)=> {setSelectedCatagory(e.target.value)}}>
+                            <MenuItem value="search-alias=all">All</MenuItem>
+                            <MenuItem value="search-alias=alexa-skills">Alexa Skills</MenuItem>
+                            <MenuItem value="search-alias=amazon-devices">Amazon Devices</MenuItem>
+                            <MenuItem value="search-alias=amazon-fashion">Amazon Fashion</MenuItem>
+                            <MenuItem value="search-alias=amazon-pharmacy">Amazon Pharmacy</MenuItem>
+                            <MenuItem value="search-alias=appliances">Appliances</MenuItem>
+                            <MenuItem value="search-alias=mobile-apps">Apps &amp; Games</MenuItem>
+                            <MenuItem value="search-alias=baby">Baby</MenuItem>
+                            <MenuItem value="search-alias=beauty">Beauty</MenuItem>
+                            <MenuItem value="search-alias=stripbooks">Books</MenuItem>
+                            <MenuItem value="search-alias=automotive">Car &amp; Motorbike</MenuItem>
+                            <MenuItem value="search-alias=apparel">Clothing &amp; Accessories</MenuItem>
+                            <MenuItem value="search-alias=collectibles">Collectibles</MenuItem>
+                            <MenuItem value="search-alias=computers">Computers &amp; Accessories</MenuItem>
+                            <MenuItem value="search-alias=todays-deals">Deals</MenuItem>
+                            <MenuItem value="search-alias=electronics">Electronics</MenuItem>
+                            <MenuItem value="search-alias=furniture">Furniture</MenuItem>
+                            <MenuItem value="search-alias=lawngarden">Garden &amp; Outdoors</MenuItem>
+                            <MenuItem value="search-alias=gift-cards">Gift Cards</MenuItem>
+                            <MenuItem value="search-alias=grocery">Grocery &amp; Gourmet Foods</MenuItem>
+                            <MenuItem value="search-alias=hpc">Health &amp; Personal Care</MenuItem>
+                            <MenuItem value="search-alias=kitchen">Home &amp; Kitchen</MenuItem>
+                            <MenuItem value="search-alias=industrial">Industrial &amp; Scientific</MenuItem>
+                            <MenuItem value="search-alias=jewelry">Jewellery</MenuItem>
+                            <MenuItem value="search-alias=digital-text">Kindle Store</MenuItem>
+                            <MenuItem value="search-alias=luggage">Luggage &amp; Bags</MenuItem>
+                            <MenuItem value="search-alias=luxury-beauty">Luxury Beauty</MenuItem>
+                            <MenuItem value="search-alias=dvd">Movies &amp; TV Shows</MenuItem>
+                            <MenuItem value="search-alias=popular">Music</MenuItem>
+                            <MenuItem value="search-alias=mi">Musical Instruments</MenuItem>
+                            <MenuItem value="search-alias=office-products">Office Products</MenuItem>
+                            <MenuItem value="search-alias=pets">Pet Supplies</MenuItem>
+                            <MenuItem value="search-alias=instant-video">Prime Video</MenuItem>
+                            <MenuItem value="search-alias=speed-store">Same-Day Delivery</MenuItem>
+                            <MenuItem value="search-alias=shoes">Shoes &amp; Handbags</MenuItem>
+                            <MenuItem value="search-alias=software">Software</MenuItem>
+                            <MenuItem value="search-alias=sporting">Sports, Fitness &amp; Outdoors</MenuItem>
+                            <MenuItem value="search-alias=specialty-aps-sns">Subscribe &amp; Save</MenuItem>
+                            <MenuItem value="search-alias=home-improvement">Tools &amp; Home Improvement</MenuItem>
+                            <MenuItem value="search-alias=toys">Toys &amp; Games</MenuItem>
+                            <MenuItem value="search-alias=under-ten-dollars">Under ₹500</MenuItem>
+                            <MenuItem value="search-alias=videogames">Video Games</MenuItem>
+                            <MenuItem value="search-alias=watches">Watches</MenuItem>
+                        </Select>
+      
                     </div>
                 </div>
 
